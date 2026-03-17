c34489fb4ad3c34859b432a313eab7220b123d9d
vpetrychv <petrychvasyl@gmail.com>
2026-03-17 16:38:34 +0100

    Final Commit

---
+++ MyToDoList/Views/TaskList/Subview/NoTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  NoTaskView.swift
+//  MyToDoList
+//
+//  Created by Vasyl Petrych on 16/03/2026.
+//
+
+import SwiftUI
+
+struct NoTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoTaskView()
+}
